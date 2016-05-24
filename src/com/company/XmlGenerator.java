95bf1a5710b3ec48e86e8f707848c6281a649f96
Paul LAFAURIE <lp406203@etu.unice.fr>
2016-05-24 17:11:54 +0200

    first shot de personalisation

--- src/com/company/XmlGenerator.java
+++ src/com/company/XmlGenerator.java
@@ -12,32 +12,34 @@ import java.nio.file.Paths;
  * Created by Renaud on 12/05/2016.
  */
 public class XmlGenerator {
+    private static String xml;
+    private static String inputPath = "C:\\Users\\user\\Documents\\Cours\\JsonXmlParser\\src\\com\\company\\log.json";
+    private static String outputPath = "C:\\Users\\user\\Documents\\Cours\\JsonXmlParser\\src\\com\\company\\output.xml";
 
-    static String readFile(String path)
+    private static String readFile(String path)
             throws IOException
     {
         byte[] encoded = Files.readAllBytes(Paths.get(path));
         return new String(encoded, "UTF-8");
     }
 
+    private static void saveToXml() throws FileNotFoundException, UnsupportedEncodingException {
+        PrintWriter writer = new PrintWriter(outputPath, "UTF-8");
+        writer.print(xml);
+        writer.close();
+    }
+
+
     public static void main(String args[]){
-        String inputPath = "C:\\Users\\user\\Documents\\Cours\\JsonXmlParser\\src\\com\\company\\log.json";
-        String outputPath = "C:\\Users\\user\\Documents\\Cours\\JsonXmlParser\\src\\com\\company\\output.xml";
+
 
         try {
 
             JSONArray json = new JSONArray(readFile(inputPath));
 
-            String xml = XML.toString(json);
-            xml = xml.replaceAll(">",">\n");
-            String log = "<log>\n";
-            xml = log.concat(xml);
-            log = "</log>";
-            xml = xml.concat(log);
+            editXML(json);
+            saveToXml();
 
-            PrintWriter writer = new PrintWriter(outputPath, "UTF-8");
-            writer.print(xml);
-            writer.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (UnsupportedEncodingException e) {
@@ -48,5 +50,21 @@ public class XmlGenerator {
             e.printStackTrace();
         }
 
+    }
+
+    private static void editXML(JSONArray json) throws  JSONException{
+        xml = XML.toString(json);
+        String log = "<?xml-stylesheet type=\"text/css\" href=\"island.css\" ?>\n<log>\n";
+        xml = log.concat(xml);
+        log = "</log>";
+        xml = xml.concat(log);
+
+        xml = xml.replaceAll("contracts", "contract");
+        xml = xml.replaceAll("(<contract>.*</contract>)", "<contracts>$1</contracts>");
+        xml = xml.replaceFirst("<array>", "<array id=\"init\">");
+        xml = xml.replaceAll("<array><data><cost>", "<array id=\"from_server\"><data><cost>");
+        xml = xml.replaceAll("<array><data><action>", "<array id=\"from_client\"><data><action>");
+
+        xml = xml.replaceAll(">",">\n");
     }
 }
